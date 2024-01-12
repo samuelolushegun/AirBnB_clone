78901b33d43408855e90200540a17fa7fc9d49e5
Samuel <etchoudinansamuel@gmail.com>
2024-01-12 16:38:57 +0000

    Update models/base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -8,11 +8,21 @@ class BaseModel:
     '''
     Basemodel class defines all common attributes and methods for other cls
     '''
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         '''Init method for Base Model'''
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if len(kwargs) == 0:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+        else:
+            d_format = "%Y-%m-%dT%H:%M:%S.%f"
+            self.created_at = datetime.strptime(kwargs['created_at'], d_format)
+            self.updated_at = datetime.strptime(kwargs['updated_at'], d_format)
+
+            for key, value in kwargs.items():
+                if key != '__class__' and key != 'created_at' and \
+                   key != 'updated_at':
+                    setattr(self, key, value)
 
     def __str__(self):
         '''string representation for my Base model class'''
